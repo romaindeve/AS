b2f5f94543780ec8ed48ccbe6e73fc3f5beec691
Romain BIGNOTTI <romain.bignotti@cs-sports.fr>
2018-06-29 12:46:18 +0200

    Initialistation

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CentraleSupélec Sports</title>
+    <meta name="description" content="Page d'accueil du Bureau des Sports de CentraleSupélec Gif-sur-Yvette">
+    <meta name="keywords" content="BDS, CentraleSupélec, Supélec, Gif-sur-Yvette, TOSS, TODS, Sixte, AS, CS, Sports, cs-sports">
+    <meta name="author" content="BDS CentraleSupélec Gif-sur-Yvette">
+<meta name="google-site-verification" content="CcM5SWqrAViqe-DHPGOORe2TyLICzPD5ioHGaufu0uU" />
+    <!-- favicons multi-platform setup (using http://realfavicongenerator.net/) -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="BDS CentraleSupélec Gif">
+    <meta name="application-name" content="BDS CentraleSupélec Gif">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.min.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu" aria-expanded="false"
+                    aria-controls="navbar">
+                    <span class="sr-only">(Dés)activer le menu de navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">CentraleSupélec Sports</a>
+            </div>
+            <div id="navbar-menu" class="navbar-collapse collapse navbar-right">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Accueil</a></li>
+                    <li><a href="index.php#section-events">Évènements</a></li>
+                    <li><a href="content/planning.php">Planning</a></li>
+                    <li><a href="docs/reglement_interne.pdf">Règlement interne</a></li>
+                    <li><a href="content/staff.php">L'équipe</a></li>
+                    <li><a href="content/parts.php">Sponsors</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sports<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+            
+                            <li><a href="#" data-toggle="modal" data-target="#Aviron">Aviron</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Bad">Badminton</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Basket">Basketball</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Boxe">Boxe française</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Danse">Danse</a></li>
+                            <!--<li><a href="#" data-toggle="modal" data-target="#Equitation">Equitation</a></li>-->
+                            <li><a href="#" data-toggle="modal" data-target="#Escalade">Escalade</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Escrime">Escrime</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Football">Football</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Handball">Handball</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Hockey">Hockey</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Judo">Judo</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Jiu-jitsu">Jiu-jitsu</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Karate">Karaté</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Kravmaga">Krav Maga</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Musculation">Musculation</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Natation">Natation</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Raid">Raid/Cross-country</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Rugby">Rugby</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Squash">Squash</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Tennis">Tennis</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#Tennist">Tennis de table</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#arc">Tir à l'arc</a></li>
+                            <!--<li><a href="#" data-toggle="modal" data-target="#Voile">Voile</a></li>-->
+                            <li><a href="#" data-toggle="modal" data-target="#Volley">Volley</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#WaterPolo">Water-polo</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="http://bds-supelec.fr/as/">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+<div id="content">
+
+    <!--******************************************************************
+    SPORTS
+    ***********************************************************************-->
+  
+  <?php include('content/sports.html'); ?>
+
+
+    <!-- ***********************************************************************
+     HEADER SECTION
+     *************************************************************************** -->
+    <section id="section-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>BIENVENUE SUR LE SITE DE CENTRALESUPÉLEC SPORTS !</h2>
+                </div>
+
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="col-md-4">
+                        <div class="col-md-12" id="header-line"></div>
+                        <div class="col-md-12"></div>
+                    </div>
+                    <div class="col-md-4"><img src="img/Logo_CSSports.png" class="img-responsive" id="header-picture"></div>
+                    <div class="col-md-4">
+                        <div class="col-md-12" id="header-line"></div>
+                        <div class="col-md-12"></div>
+                    </div>
+                </div>
+                  <div class="col-md-12">
+            
+                <h3>Les inscriptions à CentraleSupélec Sports sont sur le point d'ouvrir !</h3>
+                <p><br/> <b>Cliquez sur le bouton ci-dessous pour accéder à la plateforme d'inscription:</b>
+            </p>
+            <a href="http://bds-supelec.fr/as/" class="btn btn-theme">Plateforme CSS</a>
+        </div>
+
+            </div>
+        </div> 
+    </section>
+
+    <!-- ***********************************************************************
+     MAIN EVENTS SECTION
+     *************************************************************************** -->
+    <section id="section-events">
+        <div id="slap_fb_slider" style="display: none"><script type="text/javascript" src="http://surlapage.fr/script/facebook-slider?fb_url=https://www.facebook.com/TOSS.Supelec/?fref=ts&amp;fb_width=290&amp;fb_height=590&amp;fb_faces=true&amp;fb_stream=true&amp;fb_header=true&amp;fb_border=true&amp;fb_theme=light&amp;speed=FAST&amp;fb_pic=sign&amp;position=BOTTOM"></script><a href="http://rigolotes.fr" title="photos drôles">photos drôles</a></div>
+         <?php include('content/events.html'); ?>
+     </section>
+
+     <!-- **********************************************************************
+     SPONSORS SECTION
+     *************************************************************************** -->
+    <section id="section-sponsors">
+        <?php include('content/sponsors.html'); ?>
+     </section>
+
+     <div class="footer-push"></div>
+</div>
+
+    <footer>
+        <?php include('footer.html'); ?>
+    </footer>
+
+    <!-- jQuery loading with server backup -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.3.min.js"><\/script>')</script>
+
+    <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/jquery-cycle2.min.js"></script>
+    <script src="js/jquery-cycle2-carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
